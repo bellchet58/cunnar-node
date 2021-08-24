5c95c1c0092506deb1640d9de801128582b97aa5
liubj <liubj@belewtech.com>
2021-08-24 14:19:26 +0800

    修改rollup用于nodejs导出

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,33 @@ import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import json from '@rollup/plugin-json'
 import pkg from './package.json';
+import external from 'rollup-plugin-peer-deps-external'
 
 export default {
   input: 'src/index.ts', // 打包入口
-  output: { // 打包出口
-    file: pkg.browser, // 最终打包出来的文件路径和文件名，这里是在package.json的browser: 'dist/index.js'字段中配置的
-    format: 'cjs', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
-  },
+  output: [{ // 打包出口
+    file: pkg.main,
+    format: 'cjs', 
+    exports: 'named',
+    sourcemap: true,
+  }, {
+    file: pkg.module,
+    format: 'es',
+    exports: 'named',
+    sourcemap: true
+  }],
   plugins: [ // 打包插件
+    external(),
     resolve(), // 查找和打包node_modules中的第三方模块
-    commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+    typescript({
+      // 配置参考 https://hyeomans.com/migrate-node-package-typescript-rollup/
+      // rollupCommonJSResolveHack: true,
+      exclude: '**/__tests__/**',
+      // clean: true
+    }),// 解析TypeScript
+    commonjs({
+      include: ['node_modules/**']
+    }), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
     json(),
-    typescript() // 解析TypeScript
   ]
-}; 
+};
